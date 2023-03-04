22ff915e6ae55662e363c9f62aa22503c822ce23
ADIKryvonos <108137330+ADIKryvonos@users.noreply.github.com>
2023-03-04 15:19:56 +0200

    zmina

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -4,7 +4,37 @@ import { FiSearch } from 'react-icons/fi';
 import { FormBtn, InputSearch, SearchFormStyled } from './SearchForm.styled';
 
 export class SearchForm extends Component {
+  state = {
+    input: '',
+  };
+
+  onChange = e => {
+    this.setState({ input: e.target.value });
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+
+    this.props.searchName(this.state.input);
+    this.setState({ input: '' });
+  };
+
   render() {
-    return <h2>SearchForm</h2>;
+    const { input } = this.state;
+    return (
+      <SearchFormStyled onSubmit={this.onSubmit}>
+        <FormBtn type="submit">
+          <FiSearch size="16px" />
+        </FormBtn>
+        <InputSearch
+          value={input}
+          onChange={this.onChange}
+          placeholder="What do you want to write?"
+          name="search"
+          required
+          autoFocus
+        />
+      </SearchFormStyled>
+    );
   }
 }
--- src/tabs/Gallery.jsx
+++ src/tabs/Gallery.jsx
@@ -4,9 +4,51 @@ import * as ImageService from 'service/image-service';
 import { Button, SearchForm, Grid, GridItem, Text, CardItem } from 'components';
 
 export class Gallery extends Component {
+  state = {
+    searchName: '',
+    pictures: [],
+    page: 1,
+  };
+
+  onSubmit = searchName => {
+    this.setState({ searchName: searchName });
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (
+      prevState.searchName !== this.state.searchName ||
+      prevState.page !== this.state.page
+    ) {
+      this.getPictures(this.state.searchName, this.state.page);
+    }
+  }
+
+  getPictures = async (value, page) => {
+    try {
+      const { photos } = await ImageService.getImages(value, page);
+      this.setState({ pictures: photos });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   render() {
+    const { pictures } = this.state;
+    console.log(this.state.searchName);
     return (
       <>
+        <SearchForm searchName={this.onSubmit} />
+        <Grid>
+          {pictures.map(({ id, avg_color, alt, src }) => {
+            return (
+              <GridItem key={id}>
+                <CardItem color={avg_color}>
+                  <img src={src.large} alt={alt} />
+                </CardItem>
+              </GridItem>
+            );
+          })}
+        </Grid>
         <Text textAlign="center">Sorry. There are no images ... 😭</Text>
       </>
     );
